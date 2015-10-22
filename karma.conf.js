4d6ebf0ef6dc04c9865cfc792f746c4f0c628646
Christoffer Åhrling <christoffer@nilpath.se>
2015-10-22 22:49:55 +0200

    added karma

---
+++ karma.conf.js
@@ -0,0 +1,34 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '',
+    frameworks: ['browserify', 'mocha'],
+    plugins: [
+      'karma-mocha',
+      'karma-mocha-reporter',
+      'karma-phantomjs-launcher',
+      'karma-browserify'
+    ],
+    files: [
+      'node_modules/expect.js/index.js',
+      'test/helpers/*.js',
+      'src/**/*.tag',
+      'src/**/*.js',
+      'test/**/*.js'
+    ],
+    preprocessors: {
+      'src/**/*.tag': ['browserify'],
+      'src/**/*.js': ['browserify'],
+      'test/**/*.js': ['browserify']
+    },
+    
+    // browserify configuration
+    browserify: {
+      debug: true,
+      transform: [ 'babelify', 'riotify' ]
+    },
+    
+    browsers: ['PhantomJS'],
+    reporters: ['mocha'],
+    singleRun: true
+  });
+};
