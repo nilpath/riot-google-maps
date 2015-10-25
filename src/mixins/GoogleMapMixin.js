24150aa95295d59600f931a043554236137e7247
Christoffer Åhrling <christoffer@nilpath.se>
2015-10-25 10:26:51 +0100

    moved Google Map tag logic to a mixin

---
+++ src/mixins/GoogleMapMixin.js
@@ -0,0 +1,36 @@
+import {googleMapEvents} from '../events';
+import {composeOptions, registerEvents, unregisterEvents} from '../utils';
+
+var MAP_OPTIONS = [
+  'center', 
+  'heading', 
+  'mapTypeId',
+  'options', 
+  'streetview', 
+  'tilt',
+  'zoom'
+];
+
+export default function GoogleMapMixin() {
+    
+  this.init = function () {
+    //console.log('init GoogleMapMixin');
+  };
+  
+  this.onMount = function () {
+    console.log('mounting');
+    const mapOptions = composeOptions(MAP_OPTIONS, this.opts);
+    this.map = this.createMap(this.root, mapOptions);
+    this.registeredEvents = registerEvents(googleMapEvents, this.opts, this.map);
+  };
+  
+  this.onUnmount = function () {
+    unregisterEvents(this.registeredEvents);
+  };
+  
+  this.createMap = function (elem, options) {
+    return new google.maps.Map(elem, options);
+  };
+
+}
+
---
+++ src/mixins/index.js
@@ -0,0 +1,1 @@
+export {default as GoogleMapMixin} from './GoogleMapMixin';
