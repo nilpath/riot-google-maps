67bdd307a9f2abb81d78ecdd52f8fbde67020ee1
Christoffer Åhrling <christoffer@nilpath.se>
2016-03-01 13:51:41 +0100

    fixed broken tests

--- test/tags/GoogleMap_test.js
+++ test/tags/GoogleMap_test.js
@@ -1,11 +1,11 @@
 describe('GoogleMap Tag: ', () => {
-  
+
   var tag, mixinStub;
-  
+
   beforeEach(() => {
     var html = document.createElement('google-map');
     document.body.appendChild(html);
-    
+
     function GoogleMapMixinStub() {
       this.init = function() {
         this.on('mount', this.onMount);
@@ -16,48 +16,48 @@ describe('GoogleMap Tag: ', () => {
       this.onUnmount = sinon.stub();
       this.onUpdate = sinon.stub();
     }
-    
+
     mixinStub = new GoogleMapMixinStub();
     riot.mixin('GoogleMapMixin', mixinStub);
   });
-  
+
   afterEach(() => {
     tag.unmount();
     tag = undefined;
   });
-  
+
   it('mounts GoogleMap tag', () => {
     tag = riot.mount('google-map')[0];
     expect(tag).to.be.an(Object);
   });
-  
+
   it('should call GoogleMapMixin.onMount when mounting', () => {
     tag = riot.mount('google-map')[0];
-    
+
     expect(mixinStub.onMount.called).to.be(true);
   });
-  
+
   it('should call GoogleMapMixin.onUnmount when unmounting', () => {
     tag = riot.mount('google-map')[0];
     tag.unmount();
     expect(mixinStub.onUnmount.called).to.be(true);
   });
-  
+
   it('should call GoogleMapMixin.onUpdate when updating', () => {
     tag = riot.mount('google-map')[0];
-    
+
     tag.update();
-    
+
     expect(mixinStub.onUpdate.called).to.be(true);
   });
-  
+
   it('should update prevOpts when updated', () => {
     tag = riot.mount('google-map')[0];
     tag.opts = {key: 'value'};
-    expect(tag.prevOpts).to.eql({});
-    
+    expect(tag.prevOpts).to.eql({ style: 'overflow: hidden;' });
+
     tag.update();
     expect(tag.prevOpts).to.eql(tag.opts);
   });
-  
+
 });
