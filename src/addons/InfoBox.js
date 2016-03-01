39f4b28ad2351dddf018ec52199ef7edde6b89b6
Christoffer Åhrling <christoffer@nilpath.se>
2016-03-01 14:09:28 +0100

    Update InfoBox.js

--- src/addons/InfoBox.js
+++ src/addons/InfoBox.js
@@ -2,5 +2,7 @@ import {
   InfoBoxMixin
 } from './mixins';
 
+//http://google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/docs/reference.html
+
 riot.mixin('InfoBoxMixin', new InfoBoxMixin());
 export { default as InfoBoxTag } from './tags/InfoBox.tag';
